2b1c49a1107d8ef2a51d6117f16b419ca5a68afd
Viviane Annen <viviane.annen@gmail.com>
2017-11-25 11:33:45 +0100

    created CRUD for Admin (incl. tests)

---
+++ grails-app/domain/ch/fhnw/blog/Post.groovy
@@ -0,0 +1,18 @@
+package ch.fhnw.blog
+
+
+class Post {
+
+    String title
+    String body
+    String author
+    Date created = new Date()
+    Date published
+
+    static constraints = {
+    }
+
+    static mapping = {
+        body type: 'text'
+    }
+}
